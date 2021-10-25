9232b40249735701b4f29fcfdb0dc81287a5acd3
Danila Fedorin <danila.fedorin@gmail.com>
2021-10-25 02:17:34 -0700

    Properly scope styleS

--- src/Menu/Window.tsx
+++ src/Menu/Window.tsx
@@ -5,7 +5,7 @@ import "./Window.scss";
 
 export const Window = (props: React.PropsWithChildren<{onClose(): void }>) => {
     return (
-        <div className="menu">
+        <div className="window">
             <div className="title">
                 {COLORS.map(color => <span key={color} className={`${color} color-dot`}></span>)}
                 <span className="title-text">Matrix Highlight</span>
