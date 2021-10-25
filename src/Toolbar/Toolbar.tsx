4c3f85d53d73eeacc383d6a56dc400c7854f5b54
Danila Fedorin <danila.fedorin@gmail.com>
2021-10-25 01:27:20 -0700

    Add colorful buttons to toolbar (not hooked up)

--- src/Toolbar/Toolbar.tsx
+++ src/Toolbar/Toolbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Toolbar.scss";
+import { Cloud, Users, Folder } from "react-feather";
 
 export type ToolbarProps = {
     onOpenMenu(): void
@@ -10,6 +11,9 @@ export class Toolbar extends React.Component<ToolbarProps, {}> {
         return (
             <div className="toolbar-main">
                 <button onClick={this.props.onOpenMenu}>⋯</button>
+                <button onClick={this.props.onOpenMenu}><Cloud className="feather"/></button>
+                <button onClick={this.props.onOpenMenu}><Users className="feather"/></button>
+                <button onClick={this.props.onOpenMenu}><Folder className="feather"/></button>
             </div>
         );
     }
