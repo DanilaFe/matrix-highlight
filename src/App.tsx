3c5ec30f3f93f1943d7853ea6f37b38f65b00485
Danila Fedorin <danila.fedorin@gmail.com>
2021-10-28 22:05:03 -0700

    Use effects to render highlights

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Toolbar} from './Toolbar/Toolbar';
 import {Menu}  from "./Menu/Menu";
 import {Tooltip} from "./Tooltip/Tooltip";
-import {Room,Page, Highlight} from "./model";
+import {Page} from "./model";
+import {Renderer} from "./effects/EffectfulRenderer";
 
 const App = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -11,6 +12,14 @@ const App = () => {
     const [authTab, setAuthTab] = useState<"login" | "signup">("login");
     const [toolsTab, setToolsTab] = useState<"quotes" | "rooms" | "users">("quotes");
     const [page, setPage] = useState(new Page({}));
+
+    useEffect(() => {
+        Renderer.setActiveListener(active => {});
+    }, []);
+    
+    useEffect(() => {
+        Renderer.apply(page.currentRoom?.highlights || []);
+    });
 
     return !showMenu ?
         <div><Toolbar onOpenMenu={() => setShowMenu(true) }/><Tooltip/></div> :
