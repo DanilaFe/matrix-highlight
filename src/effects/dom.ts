e21675505bbea4101a3dbfb82db38f6bd51928a8
Danila Fedorin <danila.fedorin@gmail.com>
2021-10-28 20:27:35 -0700

    Add code for finding elements to highlight and highlighting them

---
+++ src/effects/dom.ts
@@ -0,0 +1,82 @@
+export function isHighlight(node: Node): boolean {
+    return node instanceof Element && node.hasAttribute('data-mhl-highlight');
+}
+
+export function isStructural(node: Node): boolean {
+    return node instanceof Element && node.hasAttribute('data-mhl-structural');
+}
+
+export function isInserted(node: Node): boolean {
+    return isHighlight(node) || isStructural(node);
+}
+
+export function getLength(node: Node): number {
+    if (isInserted(node)) {
+        return parseInt((node as Element).getAttribute('data-mhl-length') || "0");
+    } else if (node instanceof Text) {
+        return node.textContent?.length || 0;
+    }
+    return 0;
+}
+
+export function makeHighlightSpan(id: number | string, length: number): HTMLElement {
+    const span = document.createElement('span');
+    span.setAttribute('data-mhl-length', length.toString());
+    span.setAttribute('data-mhl-id', id.toString());
+    return span;
+}
+
+export function unpackStructural(node: Element): [ Node | undefined, Element, Node | undefined ] {
+    const id = node.getAttribute('data-mhl-id');
+    let left: Node | undefined, right: Node | undefined, highlight: Element;
+    if (node.childNodes[0] instanceof Element && node.childNodes[0].getAttribute('data-mhl-id') === id) {
+        highlight = node.childNodes[0];
+        right = node.childNodes[1];
+    } else {
+        left = node.childNodes[0];
+        highlight = node.childNodes[1] as Element;
+        right = node.childNodes[2];
+    }
+    return [left, highlight, right];
+}
+
+export function highlightTextPortion(textNode: Text, from: number, to: number, id: number | string, color: string): { highlight: HTMLElement, structural?: HTMLElement } | null {
+    if (to <= from) {
+        return null;
+    }
+    const string = textNode.textContent || "";
+    const before = string.slice(0, from);
+    const middle = string.slice(from, to);
+    const after = string.slice(to, string.length);
+
+    const structural = makeHighlightSpan(id, string.length);
+    structural.setAttribute('data-mhl-structural', "true");
+    const highlight = makeHighlightSpan(id, to-from);
+    highlight.setAttribute('data-mhl-highlight', "true");
+    highlight.classList.add("highlight");
+    highlight.classList.add(color);
+    highlight.textContent = middle;
+
+    if (before) { structural.appendChild(document.createTextNode(before)); }
+    structural.appendChild(highlight);
+    if (after) { structural.appendChild(document.createTextNode(after)); }
+
+    if (before || after) {
+        textNode.parentElement?.replaceChild(structural, textNode);
+        return { structural, highlight };
+    } else {
+        textNode.parentElement?.replaceChild(highlight, textNode);
+        return { highlight };
+    }
+}
+
+export function clearTextPortion(node: Element): void {
+    if (isStructural(node)) {
+        const [left, highlight, right] = unpackStructural(node);
+        const newNode = document.createTextNode((left?.textContent || "") + ((highlight.childNodes[0] as Text).textContent || "") + (right?.textContent || ""));
+        node.parentNode?.replaceChild(newNode, node);
+    } else {
+        node.parentNode?.replaceChild(node.childNodes[0], node);
+    }
+}
+
---
+++ src/effects/location.ts
@@ -0,0 +1,179 @@
+import {isInserted, getLength} from "./dom";
+import {
+    HIGHLIGHT_NODE_PATH_KEY, HIGHLIGHT_NODE_OFFSET_KEY,
+    HIGHLIGHT_COLOR_KEY, HIGHLIGHT_START_KEY, HIGHLIGHT_END_KEY, HIGHLIGHT_TEXT_KEY,
+    NodeData, HighlightContent
+} from "../model/matrix";
+
+export type NodePointer = {
+    node: Node,
+    offset: number | undefined
+}
+
+type Path = number[]
+
+function nodePath(node: Node): Path {
+    let parent = node.parentNode;
+    const indices = []
+    while (parent) {
+        indices.push(Array.prototype.indexOf.call(parent.childNodes, node));
+        node = parent;
+        parent = node.parentNode;
+    }
+    indices.reverse();
+    return indices;
+}
+
+function comparePaths(path1: Path, path2: Path): number {
+    for (let i = 0; i < path1.length && i < path2.length; i++) {
+        if (path1[i] > path2[i]) return 1;
+        if (path1[i] < path2[i]) return -1;
+    }
+    return Math.sign(path1.length - path2.length);
+}
+
+function recalculateOffset(node: Node, offset: number): NodePointer {
+    if (isInserted(node)) {
+        // Convert highlight nodes into offsets within text
+        offset = 0;
+    } else if (!(node instanceof Text)) {
+        // Neither a highlight nor text node; we maintain
+        // the invariant that highlights only contain other
+        // highlights, so this isn't a part of a highlight,
+        // and doesn't need to be adjusted.
+        if (offset === node.childNodes.length) {
+            offset = offset - 1;
+        }
+        return { node: node.childNodes[offset], offset: undefined }
+    }
+
+    while (node.parentNode && isInserted(node.parentNode)) {
+        // TypeScript knows parent is not null here.
+        const parent = node.parentNode;
+        while (node.previousSibling) {
+            node = node.previousSibling;
+            offset += getLength(node);
+        }
+        node = parent;
+    }
+    return { node, offset }
+}
+
+function makeNodeData(node: Node, offset: number): NodeData {
+    const { node: newNode, offset: newOffset } = recalculateOffset(node, offset);
+    const pathObject: NodeData = {
+        [HIGHLIGHT_NODE_PATH_KEY]: nodePath(newNode)
+    };
+    if (newOffset !== undefined) {
+        pathObject[HIGHLIGHT_NODE_OFFSET_KEY] = newOffset;
+    }
+    return pathObject;
+}
+
+function pickSmaller(pathObj1: NodeData, pathObj2: NodeData) {
+    const path1 = pathObj1[HIGHLIGHT_NODE_PATH_KEY];
+    const path2 = pathObj2[HIGHLIGHT_NODE_PATH_KEY];
+    const pathCmp = comparePaths(path1, path2);
+    if (pathCmp !== 0) return pathCmp;
+
+    const offset1 = pathObj1[HIGHLIGHT_NODE_OFFSET_KEY] || 0;
+    const offset2 = pathObj2[HIGHLIGHT_NODE_OFFSET_KEY] || 0;
+    return Math.sign(offset1 - offset2);
+}
+
+export function makeEvent(selection: Selection): Omit<HighlightContent, typeof HIGHLIGHT_COLOR_KEY> | null {
+    if (selection.rangeCount !== 1 || selection.type !== "Range" || !selection.anchorNode || !selection.focusNode) {
+        return null;
+    }
+    const anchorPath = makeNodeData(selection.anchorNode, selection.anchorOffset);
+    const focusPath = makeNodeData(selection.focusNode, selection.focusOffset);
+    const compare = pickSmaller(anchorPath, focusPath);
+    let smaller, bigger;
+    switch (compare) {
+        case 1:
+        case 0: // Begin and end with the same node without offset
+            smaller = focusPath;
+            bigger = anchorPath;
+            break;
+        case -1:
+            smaller = anchorPath;
+            bigger = focusPath;
+            break;
+        default:
+            return null;
+    }
+    const smallNode = followPath(smaller[HIGHLIGHT_NODE_PATH_KEY], smaller[HIGHLIGHT_NODE_OFFSET_KEY]);
+    const bigNode = followPath(bigger[HIGHLIGHT_NODE_PATH_KEY], bigger[HIGHLIGHT_NODE_OFFSET_KEY]);
+    if (!smallNode || !bigNode) {
+        return null;
+    }
+    let textPieces: string[] = [];
+    iterateTextPieces(smallNode.node, smallNode.offset, bigNode.node, bigNode.offset, (node, from, to) => {
+        textPieces.push(node.textContent?.substring(from, to) || "");
+    });
+    return {
+        [HIGHLIGHT_TEXT_KEY]: textPieces,
+        [HIGHLIGHT_START_KEY]: smaller,
+        [HIGHLIGHT_END_KEY]: bigger,
+    }
+}
+
+export function decodeNodeData(data: NodeData): NodePointer | null {
+    const offset = data[HIGHLIGHT_NODE_OFFSET_KEY]
+    return followPath(data[HIGHLIGHT_NODE_PATH_KEY], offset);
+}
+
+function followPath(path: Path, offset: number | undefined): NodePointer | null {
+    let node: Node | null = path.reduce((node, index) => node?.childNodes?.[index] || null, document.documentElement.parentNode as Node);
+    if (!node) return null;
+    if (!isInserted(node) || offset === undefined) return { node, offset };
+    while (node && isInserted(node)) {
+        node = node.childNodes[0];
+        while (node && offset > getLength(node)) {
+            offset -= getLength(node);
+            node = node.nextSibling;
+        }
+    }
+    return node && { node, offset }
+}
+
+function iterateText(fromNode: Node, toNode: Node, callback: (value: Text) => any): void {
+    let currentNode: Node | null = fromNode;
+    while (currentNode) {
+        // If we're an element, descend further down.
+        if (currentNode instanceof Element && currentNode.childNodes.length) {
+            currentNode = currentNode.childNodes[0];
+            continue;
+        } else if (currentNode instanceof Text) {
+            callback(currentNode);
+        }
+        
+        // Move on to next node in the list
+        while (currentNode && currentNode !== toNode && !currentNode.nextSibling) {
+            currentNode = currentNode.parentNode;
+        }
+        if (currentNode === toNode) break;
+        currentNode = currentNode && currentNode.nextSibling;
+    }
+}
+
+export function iterateTextPieces(fromNode: Node, fromOffset: number | undefined, toNode: Node, toOffset: number | undefined, callback: (value: Text, from: number, to: number) => any): void {
+    // Here, or elsewhere, do not assume that the assumptions regarding offset and text nodes
+    // hold, just to be more robust. Elements might have an offset, text nodes might not.
+    // Ensure both work.
+    if (fromNode === toNode && fromNode instanceof Text &&
+            fromOffset && fromNode instanceof Text &&
+            toOffset && toNode instanceof Text) {
+        callback(fromNode, fromOffset, toOffset);
+    } else {
+        iterateText(fromNode, toNode, textNode => {
+            if (textNode === fromNode && fromOffset) {
+                callback(textNode, fromOffset, textNode.textContent?.length || fromOffset);
+            } else if (textNode === toNode && toOffset) {
+                callback(textNode, 0, toOffset);
+            } else {
+                callback(textNode, 0, textNode.textContent?.length || 0);
+            }
+        });
+    }
+}
