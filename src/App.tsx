4bdf3b1c8b1a09e6dd8d10fbad375f3113eed888
Danila Fedorin <danila.fedorin@gmail.com>
2021-10-29 22:22:09 -0700

    Switch tooltip to using reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './App.css';
 import {Toolbar} from './Toolbar/Toolbar';
 import {Menu}  from "./Menu/Menu";
@@ -16,6 +16,49 @@ import {HIGHLIGHT_COLOR_KEY} from "./model/matrix";
 const Storage = new LocalStorage();
 const Auth = new MxsdkAuth(Storage);
 
+type TooltipState = {
+    selection: Selection | null;
+    target: string | number | null;
+    visible: boolean;
+    top: number;
+    left: number;
+};
+
+type TooltipEvent
+    = { type: "selection", selection: Selection | null }
+    | { type: "click", id: string | number, top: number, left: number }
+    | { type: "resize-selection" }
+    | { type: "resize-clicked", id: string | number, top: number, left: number }
+    | { type: "hide" }
+
+const selectionCoords = (selection: Selection) => {
+    const rect = selection.getRangeAt(0).getBoundingClientRect();
+    return { left: rect.left + window.scrollX, top: rect.top + window.scrollY };
+}
+
+const tooltipReducer = (state: TooltipState, event: TooltipEvent): TooltipState => {
+    if (event.type === "selection") {
+        const selection = event.selection;
+        if (!selection || selection.type !== "Range" || selection.toString() === "") {
+            return { ...state, visible: false, selection };
+        }
+        const { top, left } = selectionCoords(selection);
+        return { ...state, target: null, visible: true, top, left, selection };
+    } else if (event.type === "click") {
+        return { ...state, visible: true, target: event.id, top: event.top, left: event.left };
+    } else if (event.type == "resize-selection") {
+        if (!state.selection || state.target !== null) return state;
+        const { top, left } = selectionCoords(state.selection);
+        return { ...state, top, left };
+    } else if (event.type == "resize-clicked") {
+        if (event.id !== state.target) return state;
+        return { ...state, top: event.top, left: event.left };
+    } else if (event.type === "hide") {
+        return { ...state, visible: false };
+    }
+    return state;
+}
+
 const App = () => {
     const [showMenu, setShowMenu] = useState(false);
     const [menuMode, setMenuMode] = useState<"auth" | "tools">("tools");
@@ -24,29 +67,14 @@ const App = () => {
     const [page, setPage] = useState(new Page({}));
     const [currentRoomId, setCurrentRoomId] = useState<string | null>("!PvZRbsyWNPNzYhDvxz:matrix.org");
     const [client, setClient] = useState<Client | null>(null);
-    const [selection, setSelection] = useState<Selection | null>(null);
 
-    const [tooltipTarget, setTooltipTarget] = useState<string | number | null>(null);
-    const [showTooltip, setShowTooltip] = useState(false);
-    const [tooltipTop, setTooltipTop] = useState(0);
-    const [tooltipLeft, setTooltipLeft] = useState(0);
-
-    const updateTooltipPos = (selection: Selection) => {
-        const rect = selection.getRangeAt(0).getBoundingClientRect()
-        setTooltipLeft(rect.left + window.scrollX);
-        setTooltipTop(rect.top + window.scrollY);
-    };
-
-    const updateSelection = (selection: Selection | null) => {
-        setSelection(selection);
-        if (!selection || selection.type !== "Range" || selection.toString() === "") {
-            setShowTooltip(false);
-            return;
-        }
-        updateTooltipPos(selection);
-        setTooltipTarget(null);
-        setShowTooltip(true);
-    }
+    const [tooltip, tooltipDispatch] = useReducer(tooltipReducer, {
+        selection: null,
+        target: null,
+        visible: false,
+        top: 0,
+        left: 0
+    });
 
     const attmeptLogin = (username: string, password: string, homeserver: string) => {
         Auth.fromBasic(username, password, homeserver).then(c => {
@@ -57,8 +85,8 @@ const App = () => {
     }
 
     const makeNewHighlight = (color: string) => {
-        if (!selection || !currentRoomId || !client) return;
-        const skeletonEvent = makeEvent(selection);
+        if (!tooltip.selection || !currentRoomId || !client) return;
+        const skeletonEvent = makeEvent(tooltip.selection);
         if (skeletonEvent) {
             const event = Object.assign(skeletonEvent, { [HIGHLIGHT_COLOR_KEY]: color });
             const txnId = parseInt(Storage.getString("txnId") || "0");
@@ -72,7 +100,7 @@ const App = () => {
             }));
 
             window.getSelection()?.removeAllRanges();
-            setSelection(null);
+            tooltipDispatch({type: "hide"});
         }
     }
 
@@ -91,36 +119,36 @@ const App = () => {
         Renderer.subscribe({
             activeChange(id) {},
             click(id, top, left) {
-                setTooltipTop(top);
-                setTooltipLeft(left);
-                setTooltipTarget(id);
-                setShowTooltip(true);
+                tooltipDispatch({ type: "click", id, top, left });
+            },
+            move(id, top, left) {
+                tooltipDispatch({ type: "resize-clicked", id, top, left });
             }
         });
-    }, []);
+    }, [tooltipDispatch]);
 
     useEffect(() => {
         document.addEventListener("selectionchange", (e) => {
             const selection = window.getSelection();
             if (!selection || selection.type !== "Range" || selection.toString() === "") {
-                updateSelection(null);
+                tooltipDispatch({ type: "selection", selection: null });
             }
         });
         document.addEventListener("mouseup", (e) => {
-            updateSelection(window.getSelection());
+            tooltipDispatch({ type: "selection", selection: window.getSelection() });
             e.stopPropagation();
         });
-    }, []);
+    }, [tooltipDispatch]);
 
     useEffect(() => {
         const updateTooltip = () => {
-            if (selection) updateTooltipPos(selection);
+            tooltipDispatch({ type: "resize-selection" });
         };
         window.addEventListener("resize", updateTooltip)
         return () => {
             window.removeEventListener("resize", updateTooltip);
         }
-    }, [selection]);
+    }, [tooltipDispatch]);
 
     useEffect(() => {
         // Kick off authorization
@@ -167,12 +195,12 @@ const App = () => {
     return !showMenu ?
         <>
             <Toolbar onOpenMenu={() => setShowMenu(true) }/>
-            {showTooltip ?
+            {tooltip.visible ?
                 <Tooltip
-                    target={tooltipTarget}
+                    target={tooltip.target}
                     hide={hideHighlight}
                     highlight={makeNewHighlight}
-                    top={tooltipTop} left={tooltipLeft}/> :
+                    top={tooltip.top} left={tooltip.left}/> :
                 null}
         </> :
         <Menu currentMode={menuMode} onClose={() => setShowMenu(false)}>
