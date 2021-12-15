cec81f8fb26b97909f422412aad3750f2e92a3c4
Danila Fedorin <danila.fedorin@gmail.com>
2021-12-14 16:45:50 -0800

    Add IDs to messages and add code to highlights to add messages

--- src/common/model/Highlight.ts
+++ src/common/model/Highlight.ts
@@ -9,16 +9,30 @@ export class Highlight {
         return new Highlight(other.id, other.content, other.visible, other.active);
     }
 
+    localMessages: Message[] = [];
+    remoteMessages: Message[] = [];
+
     constructor(
         public id: string | number,
         public content: HighlightContent,
         public visible: boolean = true,
-        public active: boolean = false,
-        public messages: Message[] = []
+        public active: boolean = false
     ) {}
 
     get color(): string { return this.content[HIGHLIGHT_COLOR_KEY]; }
     get text(): string[] { return this.content[HIGHLIGHT_TEXT_KEY]; }
     get from(): NodeData { return this.content[HIGHLIGHT_START_KEY]; }
     get to(): NodeData { return this.content[HIGHLIGHT_END_KEY]; }
+
+    addLocalMessage(message: Message) {
+        this.localMessages.push(message);
+    }
+
+    addRemoteMessage(message: Message, txnId?: number) {
+        const localIndex = this.localMessages.findIndex(msg => msg.id === txnId);
+        if (localIndex !== -1) {
+            this.localMessages.splice(localIndex, 1);
+        }
+        this.remoteMessages.push(message);
+    }
 }
--- src/common/model/Message.ts
+++ src/common/model/Message.ts
@@ -1,6 +1,7 @@
 import {immerable} from "immer";
 
 export type MessageFields = {
+    id: number | string;
     plainBody?: string;
     formattedBody?: string;
     userId: string
@@ -9,11 +10,13 @@ export type MessageFields = {
 export class Message {
     [immerable] = true;
 
+    id: number | string;
     plainBody?: string;
     formattedBody?: string;
     userId: string;
 
     constructor(props: MessageFields) {
+        this.id = props.id;
         this.plainBody = props.plainBody;
         this.formattedBody = props.formattedBody;
         this.userId = props.userId;
