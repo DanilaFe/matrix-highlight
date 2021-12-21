bad5a6c19875d08d7622e189c8c4a8d4793dcf83
Danila Fedorin <danila.fedorin@gmail.com>
2021-12-20 23:54:04 -0800

    Avoid using properties because they don't serialize well to JSON

--- src/common/model/EchoStore.ts
+++ src/common/model/EchoStore.ts
@@ -3,45 +3,43 @@ export interface EchoItem {
 }
 
 export class EchoStore<T extends EchoItem> {
-    private _local: T[];
-    private _remote: T[];
+    local: T[];
+    remote: T[];
 
-    get local(): readonly T[] { return this._local; }
-    get remote(): readonly T[] { return this._remote; }
-    get all(): readonly T[] { return [...this._remote, ...this._local]; }
+    get all(): readonly T[] { return [...this.remote, ...this.local]; }
 
     constructor(local: T[], remote: T[]) {
-        this._local = local;
-        this._remote = remote;
+        this.local = local;
+        this.remote = remote;
     }
 
     addLocal(local: T) {
-        this._local.push(local);
+        this.local.push(local);
     }
 
     addRemote(remote: T, txnId: number | undefined, placeAtTop: boolean = false) {
-        const localIndex = this._local.findIndex(t => t.id === txnId);
+        const localIndex = this.local.findIndex(t => t.id === txnId);
         if (localIndex !== -1) {
-            this._local.splice(localIndex, 1);
+            this.local.splice(localIndex, 1);
         }
         if (placeAtTop) {
-            this._remote.splice(0, 0, remote);
+            this.remote.splice(0, 0, remote);
         } else {
-            this._remote.push(remote);
+            this.remote.push(remote);
         }
     }
 
     get(itemId: number | string): T | undefined {
-        const local = this._local.find(it => it.id === itemId);
+        const local = this.local.find(it => it.id === itemId);
         if (local) return local;
-        return this._remote.find(it => it.id === itemId);
+        return this.remote.find(it => it.id === itemId);
     }
 
     change(itemId: number | string, change: (item: T) => void) {
         const doTransform = (it: T) => {
             if (it.id === itemId) change(it);
         }
-        this._local.forEach(doTransform);
-        this._remote.forEach(doTransform);
+        this.local.forEach(doTransform);
+        this.remote.forEach(doTransform);
     }
 }
