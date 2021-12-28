7d44baefa3f3271e9e78034a39cbb33c8dff37cc
Danila Fedorin <danila.fedorin@gmail.com>
2021-12-27 17:53:31 -0800

    Restyle quotes (also to include comment buttons)

--- src/content/ToolsMenu/QuoteList.tsx
+++ src/content/ToolsMenu/QuoteList.tsx
@@ -1,13 +1,31 @@
 import {Highlight} from "../../common/model";
 import "./QuoteList.scss";
+import {MessageSquare} from "react-feather";
 
 export type QuoteListProps = {
     highlights: readonly Highlight[];
 };
 
+export const QuoteButtons = (props: { hl: Highlight }) => {
+    if (props.hl.messages.length === 0) return null;
+    return (
+        <div className="input-group">
+            <button className="labeled-icon-button">
+                <MessageSquare className="feather"/>{props.hl.messages.length} Comments
+            </button>
+        </div>
+    );
+}
+
 export const QuoteList = (props: QuoteListProps) => {
     const quoteViews = props.highlights.filter(hl => hl.visible).map(hl =>
-        <div key={hl.id} className={`quote ${hl.color}`}>{hl.text.map(s => s.trim()).join(" ")}</div>
+        <div key={hl.id} className={`quote ${hl.color}`}>
+            <span className="quote-color"/>
+            <div className="quote-data">
+                <div className="quote-text">{hl.text.map(s => s.trim()).join(" ")}</div>
+                <QuoteButtons hl={hl}/>
+            </div>
+        </div>
     );
     return (
         <div className="quote-list">
