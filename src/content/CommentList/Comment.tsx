2a97316b74848acfbb7dd3a7423beb132804a961
Danila Fedorin <danila.fedorin@gmail.com>
2022-03-19 14:51:04 -0700

    Use proper class names for comments

--- src/content/CommentList/Comment.tsx
+++ src/content/CommentList/Comment.tsx
@@ -2,6 +2,7 @@ import {Message, User} from "../../common/model";
 import sanitizeHtml from 'sanitize-html';
 import { useContext } from "react";
 import {AppContext} from "../AppContext";
+import styles from "./CommentList.scss";
 
 export type CommentProps = {
     message: Message;
@@ -11,8 +12,8 @@ export const Comment = (props: CommentProps) => {
     const users = useContext(AppContext).currentRoom?.users || [];
     const user = users.find(u => u.id === props.message.userId);
     return (
-        <div className="comment" key={props.message.id}>
-            <div className="sender">{user?.name || props.message.userId}:</div>
+        <div className={styles.comment} key={props.message.id}>
+            <div className={styles.sender}>{user?.name || props.message.userId}:</div>
             { props.message.formattedBody ?
                 <div dangerouslySetInnerHTML={{__html: sanitizeHtml(props.message.formattedBody) }}></div> :
                 <p>{props.message.plainBody}</p> }
