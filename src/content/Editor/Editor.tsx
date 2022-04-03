0f492bbbdd194d5c9374f7020ed9b54362136f29
Danila Fedorin <danila.fedorin@gmail.com>
2022-04-03 15:51:28 -0700

    Remove old Draft-based code

--- src/content/Editor/Editor.tsx
+++ src/content/Editor/Editor.tsx
@@ -53,37 +53,4 @@ export const Editor = (props: { sendReply(plain: string, formatted: string): voi
             <EditorContent editor={editor} />
         </div>
     );
-//    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-//    const [focused, setFocused] = useState(false);
-//    const handleKeyCommand = (command: string, editorState: EditorState) => {
-//      const newState = RichUtils.handleKeyCommand(editorState, command);
-//      if (newState) {
-//          setEditorState(newState);
-//          return 'handled';
-//      } else if (command === SEND_COMMAND) {
-//          const plainText = editorState.getCurrentContent().getPlainText();
-//          const formattedText = sanitizeHtml(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-//          props.sendReply(plainText, formattedText);
-//          setEditorState(EditorState.push(editorState, ContentState.createFromText(""), 'remove-range'));
-//          return 'handled'
-//      }
-//      return 'not-handled';
-//    }
-//    const toggleStyle = (style: string) => setEditorState(RichUtils.toggleInlineStyle(editorState, style));
-//    const inlineStyles = editorState.getCurrentInlineStyle();
-//    const buttons = EDITOR_BUTTONS.map(([string, icon]) =>
-//        <EditorButton toggleStyle={toggleStyle} currentStyles={inlineStyles} style={string} icon={icon}/>);
-//    return (
-//      <div className={`editor ${focused ? "focused" : ""}`}>
-//          <div className="editor-buttons">{buttons}</div>
-//          <div className={`editor-box`}>
-//              <Draft.Editor keyBindingFn={keyBindingFn}
-//                  onFocus={() => setFocused(true)}
-//                  onBlur={() => setFocused(false)}
-//                  editorState={editorState}
-//                  onChange={setEditorState}
-//                  handleKeyCommand={handleKeyCommand}/>
-//          </div>
-//      </div>
-//    );
 }
